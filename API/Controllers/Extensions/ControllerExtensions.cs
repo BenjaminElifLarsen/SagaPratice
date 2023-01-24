acd6b9dbd9c82d7da9618a19d3c36b045c47e836
Benjamin <beela93@hotmail.com>
2023-01-24 10:39:54 +0100

    corrected a naming policy mistake

--- API/Controllers/Extensions/ControllerExtensions.cs
+++ API/Controllers/Extensions/ControllerExtensions.cs
@@ -10,11 +10,11 @@ public static class ControllerExtensions
     {
         return result.ResultType switch
         {
-            ResultTypes.Success => controller.Ok(result.Data),
-            ResultTypes.SuccessNotData => controller.NoContent(),
-            ResultTypes.Invalid => controller.BadRequest(result.Errors),
-            ResultTypes.NotFound => controller.NotFound(result.Errors),
-            ResultTypes.Unexpected => controller.BadRequest(result.Errors),
+            ResultType.Success => controller.Ok(result.Data),
+            ResultType.SuccessNotData => controller.NoContent(),
+            ResultType.Invalid => controller.BadRequest(result.Errors),
+            ResultType.NotFound => controller.NotFound(result.Errors),
+            ResultType.Unexpected => controller.BadRequest(result.Errors),
             _ => throw new Exception("An unhandled result has occured. Please inform the developers that something has occured."), // Only thrown if the int value of ResultType is not a part of the ResultTypes. This should never be hit. 
         };
     }
